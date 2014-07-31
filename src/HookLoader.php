7a0c2ce77fc36f2bd60c26a497f03e06f7f9dd93
sseidelmann <sebastian.seidelmann@twt.de>
2014-07-31 17:48:37 +0200

    ADDED the raw output

--- src/HookLoader.php
+++ src/HookLoader.php
@@ -153,13 +153,13 @@ class HookLoader {
             $commandLsTree = sprintf('git ls-tree %s %s  2> /dev/null', trim($this->argvInput[5]), $file);
             exec($commandLsTree, $tree, $return);
 
-            if (count($tree)) {
+            if (count($tree) < 1) {
                 /*$tree = array(
                     1 => '4b825dc642cb6eb9a060e54bf8d69288fbee4904',
                     2 => $file
                 );*/
-            } else {
-
+                $commandLsTree = sprintf('git ls-tree %s %s  2> /dev/null', '4b825dc642cb6eb9a060e54bf8d69288fbee4904', $file);
+                exec($commandLsTree, $tree, $return);
             }
             //
             $tree = preg_split('/\s/', $tree[0]);
