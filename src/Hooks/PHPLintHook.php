bce7e19e6f05f92302fab0602f1963b87215b03d
sseidelmann <sebastian.seidelmann@twt.de>
2014-07-31 17:00:27 +0200

    ADDED the raw output

--- src/Hooks/PHPLintHook.php
+++ src/Hooks/PHPLintHook.php
@@ -25,18 +25,24 @@ class PHPLintHook extends AbstractHook {
 
             $this->logger()->debug('Checking ' . $file->getName());
             $this->logger()->write('   isValidExtension: ' . $file->isValidExtension('php'));
-
+/*
             $tmp = tempnam('/tmp/', 'phplint');
             file_put_contents($tmp, $file->getContent());
             echo "--------".PHP_EOL;
             @exec('php -l ' . $tmp, $return);
             echo "--------".PHP_EOL;
-
             foreach ($return as $line) {
                 $this->addError($file, $line);
             }
 
             unlink($tmp);
+
+*/
+            $result = exec(sprintf('echo %s | php -l', escapeshellarg($file->getContent())), $output);
+            echo $result . PHP_EOL;
+            print_r($output);
+
+
         }
 
 
