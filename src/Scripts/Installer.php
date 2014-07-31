8cf54318812dd0dc2f44678ed29bacf43df96939
sseidelmann <sebastian.seidelmann@twt.de>
2014-07-31 12:06:05 +0200

    ADDED some debug output

--- src/Scripts/Installer.php
+++ src/Scripts/Installer.php
@@ -10,32 +10,15 @@ namespace GitHooks\Scripts;
 
 use Composer\Script\Event;
 
-error_log('Loaded file ' . __FILE__);
 
 class Installer {
 
-    public static function postPackageInstall(Event $event) {
-        $event->getIO()->write('Executing the event "postPackageInstall"');
-        echo 'Executing the event "postPackageInstall"' . PHP_EOL;
-    }
-
-    public static function postPackageUpdate(Event $event) {
-        $event->getIO()->write('Executing the event "postPackageUpdate"');
-        echo 'Executing the event "postPackageUpdate"' . PHP_EOL;
-    }
-
     public static function postUpdateCmd(Event $event) {
-        $event->getIO()->write('Executing the event "postUpdateCmd"');
-        echo 'Executing the event "postUpdateCmd"' . PHP_EOL;
+        $io = $event->getIO();
+        $io->write('Executing the event "postUpdateCmd"');
+
+        print_r($_SERVER);
+        $io->write(getcwd());
     }
-
-
-    public static function genericEventCall(Event $event) {
-        $event->getIO()->write(sprintf('Executing the event "%s" (generic event)', $event->getName()));
-        echo sprintf('Executing the event "%s" (generic event)', $event->getName()) . PHP_EOL;
-        error_log(sprintf('Executing the event "%s" (generic event)', $event->getName()));
-    }
-
-
 
 }
