da5a7bc193799a52fe4f18c37e1c881c3a8c44a8
sseidelmann <sebastian.seidelmann@twt.de>
2014-07-31 10:57:04 +0200

    ADDED new installer script

---
+++ src/GitHooks/Scripts/Installer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sseidelmann
+ * Date: 31.07.14
+ * Time: 10:50
+ */
+
+namespace GitHooks\Scripts;
+
+use Composer\Script\Event;
+
+class Installer {
+
+    public static function postPackageInstall(Event $event) {
+        $event->getIO()->write('Executing the event "postPackageInstall"');
+    }
+
+}
