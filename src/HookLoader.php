fa2a5b5387c80ea401cf58f6d18c25468fc6d4be
sseidelmann <sebastian.seidelmann@twt.de>
2014-07-31 17:29:34 +0200

    ADDED empty tree items

--- src/HookLoader.php
+++ src/HookLoader.php
@@ -151,10 +151,18 @@ class HookLoader {
             $tree = array();
             \GitHooks\Helper\ConsoleOutput::logger()->debug($file);
             $commandLsTree = sprintf('git ls-tree %s %s  2> /dev/null', trim($this->argvInput[5]), $file);
-            echo $commandLsTree . PHP_EOL;
             exec($commandLsTree, $tree, $return);
-            print_r($tree);
-            $tree = preg_split('/\s/', $tree[0]);
+
+            if (count($tree)) {
+                $tree = array(
+                    1 => '4b825dc642cb6eb9a060e54bf8d69288fbee4904',
+                    2 => $file
+                );
+            } else {
+                $tree = preg_split('/\s/', $tree[0]);
+            }
+            //
+
 
 
             $fileContents = array();
