2c2707c024d3abf8b3d16b6926c0a93dfdfafdc4
sseidelmann <sebastian.seidelmann@twt.de>
2014-08-01 11:16:07 +0200

    ADDED finished command

--- src/Hooks/PHPCSHook.php
+++ src/Hooks/PHPCSHook.php
@@ -13,6 +13,14 @@ use GitHooks\AbstractHook;
 class PHPCSHook extends AbstractHook {
 
     /**
+     * Defines the default standard.
+     *
+     * @author Sebastian Seidelmann <sebastian.seidelmann@twt.de>
+     * @var    string
+     */
+    const DEFAULT_STANDARD = 'PSR2';
+
+    /**
      * Starts the hook.
      *
      * @return mixed
@@ -21,7 +29,58 @@ class PHPCSHook extends AbstractHook {
     public function run() {
 
 
+        foreach ($this->getFiles() as $file) {
+            if ($file->isValidExtension('php')) {
+
+                $tempname = tempnam('/tmp/', 'phpcs') . '.php';
+                file_put_contents($tempname, $file->getContent());
+
+                $command = sprintf(
+                    '%s --standard=%s',
+                    $this->getPHPCSExecutablePath(),
+                    $this->getStandard()
+                );
+
+                $this->addError($file, $command);
+                unlink($tempname);
+            }
+        }
+
+
         return false;
+    }
+
+    /**
+     * Returns the path to executable.
+     *
+     * @return string
+     * @author Sebastian Seidelmann <sebastian.seidelmann@twt.de>
+     */
+    private function getPHPCSExecutablePath() {
+        return VENDOR_DIRECTORY . 'bin' . DIRECTORY_SEPARATOR . 'phpcs';
+    }
+
+
+    /**
+     * Returns the CS Standard.
+     *
+     * @return string
+     * @author Sebastian Seidelmann <sebastian.seidelmann@twt.de>
+     */
+    private function getStandard() {
+
+        $standard = $this->getConfig('standard');
+        if (!$standard) {
+            $standard == self::DEFAULT_STANDARD;
+        }
+
+        exec(sprintf('%s -i', $this->getPHPCSExecutablePath()), $output);
+        $standardsInstalled = explode(' ', str_replace('The installed coding standards are ', '', $output[0]));
+        if (!in_array($standard, $standardsInstalled)) {
+            $standard = $standardsInstalled[0];
+        }
+
+        return $standard;
     }
 
 }
