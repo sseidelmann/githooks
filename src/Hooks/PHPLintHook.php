50b3bb447ebafaea53d8bc867b73f941ffee90e3
sseidelmann <sebastian.seidelmann@twt.de>
2014-08-06 17:24:24 +0200

    ADDED file input for 'php -l'

--- src/Hooks/PHPLintHook.php
+++ src/Hooks/PHPLintHook.php
@@ -32,14 +32,22 @@ class PHPLintHook extends AbstractHook {
         foreach ($this->getFiles() as $file) {
             if ($file->isValidExtension('php') && !in_array($file, self::$checked)) {
 
+
+                $tempname = tempnam('/tmp/', 'phpcs') . '.php';
+                $contents = $file->getContent();
+                file_put_contents($tempname, $contents);
+
                 $output = array();
-                $result = exec(sprintf('echo %s | php -l 2>&1', escapeshellarg($file->getContent())), $output);
+                // $result = exec(sprintf('echo %s | php -l 2>&1', escapeshellarg($file->getContent())), $output);
+                $result = exec(sprintf('php -l %s 2>&1', $tempname), $output);
                 if (strpos($result, 'Errors parsing') !== false) {
                     array_pop($output);
                     foreach ($output as $line) {
                         $this->addError($file, $line);
                     }
                 }
+
+                unset($tempname);
 
                 self::$checked[] = $file;
             }
